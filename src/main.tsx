31785cfe2efed3ab7067a26e4557e26e880e7122
Azmi Nailal Hadi <azminailalhadi28@gmail.com>
2025-03-03 13:47:40 +0700

    Perbaikan Untuk Deploy Github 2

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import './index.css';
+import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
